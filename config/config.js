a185bb85c4a9118deff4566723a0322470018a77
raram2 <gkfka1189@gmail.com>
2020-05-20 17:39:38 +0900

    Default server directory

---
+++ config/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  development: {
+    port: 4611,
+    username: '',
+    password: process.env.DB_PASSWORD,
+    database: '',
+    host: '',
+    dialect: 'mysql',
+    logging: false,
+  },
+};
