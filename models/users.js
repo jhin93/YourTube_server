74829de67d3f7c55359a12e74655aea72565abde
raram2 <gkfka1189@gmail.com>
2020-05-22 11:36:30 +0900

    Updated models, seeders

--- models/users.js
+++ models/users.js
@@ -5,7 +5,7 @@ module.exports = (sequelize, DataTypes) => {
     {
       name: DataTypes.STRING,
       channelId: DataTypes.STRING,
-      playlistId: DataTypes.STRING,
+      likePlaylistId: DataTypes.STRING,
     },
     {}
   );
