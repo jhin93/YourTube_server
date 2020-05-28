c8a75b30a06d0921331da6ad5664a13f73cc2ba1
zpxlffjrm <scm0222@naver.com>
2020-05-28 22:55:32 +0900

    search.js 가 더이상 인덱스용 파일을 리턴하지 않고 타이틀, 설명, 섬네일, 채널아이디만 리턴하게 됩니다.

--- controller/resource/search.js
+++ controller/resource/search.js
@@ -1,32 +1,53 @@
-const request = require('request');
+const request = require('request-promise-native');
 const { user } = require('../../models');
 module.exports = async (req, res) => {
   if (!req.session.accessToken) res.status(401).send();
   else {
     let { email } = req.cookies.profile;
 
-    let userid = await user.findOne({ where: { userId: userid } });
+    let userid = await user.findOne({ where: { email: email } });
     if (!userid.dataValues) res.status(404).send();
     else {
       userid = userid.dataValues.id;
 
-      let queryData = request.get(
-        'http://110.14.118.28:9200/rdbms_sync_idx/_search',
-        {
-          query: {
-            bool: {
-              should: [
-                { match: { description: req.body.search } },
-                { match: { title: req.body.search } },
-              ],
-              must: [{ match: { userid: userid } }],
+      request
+        .post('http://110.14.118.28:9200/rdbms_sync_idx/_search', {
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({
+            query: {
+              bool: {
+                should: [
+                  { match: { title: req.body } },
+                  { match: { description: req.body } },
+                ],
+              },
             },
-          },
-        }
-      );
+            _source: [
+              'channelid',
+              'title',
+              'description',
+              'thumbnail',
+              'userid',
+            ],
+          }),
+        })
+        .then((data) => JSON.parse(data))
+        .then((data) => {
+          //console.log(data.hits.hits); //todo 유저아이디로 걸러내기
 
-      res.send(queryData);
+          if (data.hits.hits.length === 0)
+            res.status(400).send('no match found');
+          else {
+            let result = [];
+            data.hits.hits.forEach((index) => {
+              console.log(index);
+              if (index._source.userid === userid) result.push(index._source);
+            });
+
+            res.status(200).json(result);
+          }
+        });
     }
   }
 };
-//http:// 110.14.118.28:9200/rdbms_sync_idx/_search?pretty -H Content-Type:application/json -d '{"query":{"bool":{"should":[{"match":{"description":"윤하"}},{"match":{"userid":1}}]}}}'
+// '{"query":{"bool":{"should":[{"match":{"description":"윤하"}}],"must":[{"match":{"userid":1}}]}}}'
