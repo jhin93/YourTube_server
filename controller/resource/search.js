12f849fe056756492d37bff0cd9fb5783fe01000
zpxlffjrm <scm0222@naver.com>
2020-05-28 20:52:10 +0900

    search.js added

--- controller/resource/search.js
+++ controller/resource/search.js
@@ -1,1 +1,31 @@
-module.exports = async (req, res) => {};
+const request = require('request');
+const { user } = require('../../models');
+module.exports = async (req, res) => {
+  if (!req.session.accessToken) res.status(401).send();
+  else {
+    let { email } = req.cookies.profile;
+
+    let userid = await user.findOne({ where: { userId: userid } });
+    if (!userid.dataValues) res.status(404).send();
+    else {
+      userid = userid.dataValues.id;
+
+      let queryData = await request.get(
+        'http://110.14.118.28:9200/rdbms_sync_idx/_search',
+        {
+          query: {
+            bool: {
+              should: [
+                { match: { description: req.body.search } },
+                { match: { userid: userid } },
+              ],
+            },
+          },
+        }
+      );
+
+      res.send(queryData);
+    }
+  }
+};
+//http:// 110.14.118.28:9200/rdbms_sync_idx/_search?pretty -H Content-Type:application/json -d '{"query":{"bool":{"should":[{"match":{"description":"윤하"}},{"match":{"userid":1}}]}}}'
