1ecff1370dcdf6ed2a7c26f0cb4491b2eb7bdaa5
raram2 <gkfka1189@gmail.com>
2020-05-29 02:09:06 +0900

    Updated config.js

--- config/config.js
+++ config/config.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOSTNAME,
-    database: 'yourtube_test',
+    username: process.env.RDS_USERNAME,
+    password: process.env.RDS_PASSWORD,
+    host: process.env.RDS_HOSTNAME,
+    database: 'yourtube_development',
     dialect: 'mysql',
   },
   production: {
